90b25bab499ffaa565884be8d0b22d2c0e7b813a
Blake Jones <43562501+blakemj@users.noreply.github.com>
2019-07-30 16:57:02 -0700

    Add files via upload

---
+++ New_Desk_Summer_2019/radio_reciever_TEST.ino
@@ -0,0 +1,38 @@
+/*
+  Arduino Wireless Network - Multiple NRF24L01 Tutorial
+          == Base/ Master Node 00==
+  by Dejan, www.HowToMechatronics.com
+  Libraries:
+  nRF24/RF24, https://github.com/nRF24/RF24
+  nRF24/RF24Network, https://github.com/nRF24/RF24Network
+*/
+
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+
+RF24 radio(10, 9);               // nRF24L01 (CE,CSN)
+RF24Network network(radio);      // Include the radio in the network
+const uint16_t this_node = 00;   // Address of this node in Octal format ( 04,031, etc)
+const uint16_t node01 = 01;      // Address of the other node in Octal format
+const uint16_t node02 = 02;
+
+void setup() {
+  SPI.begin();
+  radio.begin();
+  network.begin(90, this_node);  //(channel, node address)
+  radio.setDataRate(RF24_2MBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  Serial.begin(9600);
+}
+
+void loop() {
+  network.update();
+  //===== Receiving =====//
+  while ( network.available() ) {     // Is there any incoming data?
+    RF24NetworkHeader header;
+    char text[32] = "Fail";
+    network.read(header, &text, sizeof(text)); // Read the incoming data
+    Serial.println(text);
+  }
+} 
